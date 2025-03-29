fdf928b67c9795584131fd79691f51347e4ef9d1
user <thestrongest017@gmail.com>
2025-03-29 20:52:19 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число:");
+        int num1 = scanner.nextInt();
+
+        String sym;
+
+
+        while (true) {
+
+            System.out.println("Введите математическую операцию - (+,-,*,/)");
+            sym = scanner.next();
+
+            if (Objects.equals(sym,"+") || Objects.equals(sym,"-") || Objects.equals(sym, "*") || Objects.equals(sym, "/")) {
+                break;
+            } else {
+                System.out.println("\n"+"Нельзя ввести такой символ! <<" +sym+ ">>" +"\n");
+            }
+        }
+
+//        if (!Objects.equals(sym, "+") && !Objects.equals(sym, "-") && !Objects.equals(sym, "*") && !Objects.equals(sym, "/")) {
+//            System.out.println("Нельзя вести такой символ" + " <<"+ sym+">> " + "\n");
+//            return;
+//        }
+
+
+        int num2;
+
+        while (true) {
+
+            System.out.println("Введите второе число:"+"\n");
+            num2 = scanner.nextInt();
+
+            if (!Objects.equals(sym,"/" ) || num2 != 0) {
+                break;
+            } else {
+                System.out.println("Ошибка: нелься делить на ноль! ");
+            }
+        }
+
+        if (sym.equals("+")) {
+            int summa = num1 + num2;
+            System.out.println(num1 + " + " + num2 + " = " + summa);
+        } else if (sym.equals("-")) {
+            int difference = num1 - num2;
+            System.out.println(num1 + " - " + num2 + " = " + difference);
+        } else if (sym.equals("*")) {
+            int multiply = num1 * num2;
+            System.out.println(num1 + " * " + num2 + " = " + multiply);
+        } else {
+            double division = (double) num1 / num2;
+            System.out.println(num1 + " / " + num2 + " = " + division);
+        }
+        scanner.close();
+    }
+}
